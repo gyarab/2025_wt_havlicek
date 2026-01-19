80ea709efa07dd3afdbe095dcbdcd230cb739999
Dan <daniel.havlicek.s@gyarab.cz>
2026-01-19 19:09:51 +0100

    Domaci Ukol

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,6 +1,7 @@
 import httpx
 
 r = httpx.get("https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt")
-lines = r.text.splitlines("\n")
-
-print(lines[0])  # Tisk prvního řádku jako kontrola
+print(r.text)
+print(r.status)
+lines = r.text.split('\n')
+print(lines[0])
